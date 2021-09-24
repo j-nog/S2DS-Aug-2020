00b8b1e19f325faf53e60ddb80de929b010f96fd
João Nogueira <49312429+j-nog@users.noreply.github.com>
2021-09-24 17:20:00 +0100

    Create pdf_page_analysis.py

---
+++ nationbetter/nationbetter/pdf_page_analysis.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+from nationbetter.pdf_page_parsing import get_lines_and_info
+from nationbetter.pdf_page_analysis import foo, bar
+
+df_annot, df_content, df_data = pdf_page_parsing.build_pdf_df("abc.pdf")
+"""
+
+import pandas as pd
+
+def foo():
+    pass
+
+def bar():
+    pass
