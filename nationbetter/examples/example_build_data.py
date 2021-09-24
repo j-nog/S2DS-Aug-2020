4af8fa9ab7e4f61bfcb0b60bd1276c1a3781bdd2
João Nogueira <49312429+j-nog@users.noreply.github.com>
2021-09-24 17:19:40 +0100

    Create example_build_data.py

---
+++ nationbetter/examples/example_build_data.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os
+import pandas as pd
+import platform
+import re
+import nationbetter
+import time
+
+dirname = os.path.dirname(os.path.abspath(os.path.join(__file__,'..')))
+#Reading data files.
+pdf_urls=nationbetter.read_txt(os.path.join(dirname,'pdf_urls.txt'))
+urls=nationbetter.read_txt(os.path.join(dirname,'urls.txt'))
+#Creating a folder to store the data (does no overwrite)
+#path = nationbetter.get_data_folder()
+path = nationbetter.get_data_folder(os.path.abspath(\
+        os.path.join(__file__,'..','..','..','..')))
+path = os.path.join(path,'nationbetter_data')
+#Download the pdfs into raw_pdf folder
+start = time.time()
+#Extracting line for line text pdf (pdfminer)
+for url in pdf_urls:
+    nationbetter.get_lines_and_info(url,path)
+print('Import finished at:')
+print('\n -----------{}------------\n'.format(time.time()-start))
+
+#Download & extract raw HTML data
+for url in urls:
+    nationbetter.scrape_govuk_guidance(url,path)    
+
+#For each extracted dict build formatted dataframe
+start = time.time()
+stored_pdf = os.listdir(os.path.join(path,'raw_pdf_dicts'))[1:]
+for name in stored_pdf:
+    name = os.path.join(path,'raw_pdf_dicts',name)
+    nationbetter.pdf_dict_to_outputformat(name,path)
+print('Transform finished at:')
+print('\n -----------{}------------\n'.format(time.time()-start))
+
+stored_html = os.listdir(os.path.join(path,'raw_html_dicts'))[1:]
+for name in stored_html:
+    name = os.path.join(path,'raw_html_dicts',name)
+    nationbetter.build_segments_df(name,path)
+# # perform page analysis, disabeled in current version
+# df_annot, df_content, df_data = pdf_page_parsing.build_pdf_df(file_name[1])
