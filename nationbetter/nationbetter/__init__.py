d32fca95d138230083c5f2575b8a1c296b704381
João Nogueira <49312429+j-nog@users.noreply.github.com>
2021-09-24 17:19:45 +0100

    Create __init__.py

---
+++ nationbetter/nationbetter/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from .file_handler import *
+from .pdf_layout_parsing import *
+from .pdf_layout_analysis import *
+from .pdf_page_parsing import *
+from .pdf_page_analysis import *
+from .html_layout_parsing import *
+from .html_layout_analysis import *
+
+
