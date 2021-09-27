0cc53027b2db4ee07ff22e642f01b6d06c65e0b4
João Nogueira <49312429+j-nog@users.noreply.github.com>
2021-09-27 12:44:02 +0100

    Create keywords.py

---
+++ NLP/eyzo_NLP_tryout/keywords.py
@@ -0,0 +1,113 @@
+# lists and dictionaries of all the keywords/labels/tags
+
+visa_list =['Start-up', 'Innovator','Global talent',
+            'Student','Child Student','Temporary Worker','Youth Mobility Scheme',
+            'Business and Commercial work permits','Representative of an Overseas Business',
+            'Science and Engineering Graduate Scheme','International Graduates Scheme',
+            'Tier 4 (General)', 'Tier 2 (General)','Tier 1 (General)',
+            'Tier 5','Tier 4', 'Tier 3','Tier 2','Tier 1',]
+
+visa_dict ={
+'Tier 2 (Intra-Company Transfer): Established Staff' : ['Established Staff'],
+'Tier 2 (Intra-Company Transfer): Graduate Trainee' : ['Graduate Trainee'],
+'Tier 2 (Intra-Company Transfer): Long Term staff' : ['Long Term staff'],
+'Tier 2 (Intra-Company Transfer): Short Term staff' : ['Short Term staff'],
+'Tier 2 (Intra-Company Transfer): Skills Transfer' : ['Skills Transfer'],
+'Tier 2 (Minister of Religion)' : ['Minister of Religion'],
+'Tier 2 (Sportsperson)'  : ['Sportsperson'],
+'Tier 1 (Entrepreneur)' : ['Enterpreneur'],
+'Tier 1 (Exceptional Talent)' : ['Exceptional Talent'],
+'Tier 1 (Graduate Entrepreneur)' : ['Graduate Entrepreneur'],
+'Tier 1 (Investor)' : ['Investor'],
+'Representative of an Overseas Newspaper, News Agency or Broadcaster'  : ['Overseas Newspaper', 'News Agency', 'Broadcaster']
+}
+company_list = ['Branch Office of an Overseas Company',
+'Charity',
+'Company Limited by Guarantee',
+'Incorporated by Royal Charter',
+'Limited Liability Partnership',
+'Limited Partnership',
+'Public Limited Company',
+'Representative Office of an Overseas Company',
+'Sole Trader',
+'Unlimited Company', 
+'Co Inc under the Industrial & Provident Societies Act 1968',
+'Private Individual']
+
+regbody_list = ['Ofsted','Care Quality Commission','Financial Conduct Authority','Prudential Regulation Authority',
+'General Medical Council', 'General Dental Council']
+
+region_list = ['Commonwealth', 'non-EEA', 'EEA', 'British overseas territories', 'Isle of Man','stateless', 'refugee']
+region_dict = {
+    'EEA' : ['European Economic Area'],
+    'UK regions' : ['England', 'Wales', 'Scotland', 'Northern Ireland'],
+    'Channel Islands' : ['Guernsey', 'Jersey', 'Channel Islands'],
+    'British Overseas Territories' : ['Anguilla','Bermuda', 'British Antarctic Territory', 'British Indian Ocean Territory',
+                                    'British Virgin Islands', 'Cayman Islands', 'Falkland Islands', 'Gibraltar', 'Montserrat',
+                                     'Pitcairn', 'Saint Helena', 'Ascension', 'Tristan da Cunha', 'South Georgia', 'Akrotiri', 
+                                    'Dhekelia', 'Turks and Caicos Islands'],
+    'Majority English Speaking Countries' : ['Antigua and Barbuda', 'Australia','Bahamas','Barbados','Belize','Canada','Dominica','Grenada','Guyana',
+'Jamaica','New Zealand','Ireland', 'St Kitts and Nevis','St Lucia','St Vincent and the Grenadines','Trinidad and Tobago','USA', 'United States']
+}
+
+company_dict = {
+    'Central/local Gov. Dept; Body; Quango, Agency Public Body' : ['Central Gov. Dept.', 'Central Government Department',
+                                                                   'Local Gov. Dept.', 'Central Government Department',
+                                                                   'Quango', 'Agency Public Body'],
+   'Hotel and/or Catering Establishment' : ['Hotel Establishment', 'Catering Establishment'],
+    'Private Limited Company/ Limited Liability Company' : ['Private Limited Company' 'Limited Liability Company'],
+    'Religious and/or Faith Body' : ['Religious Body', 'Faith Body'],
+    'Overseas Gov.Int.Organisation, Diplomatic mission' : ['Overseas Gov. Int. Organisation', 'Overseas Gov.Int.Organisation',
+                                                           'Diplomatic mission']
+}
+
+compbody_list = ['company body', 'Single body', 'Group of Branches', 'Franchise', 'United Kingdom branch']
+compbody_dict = {
+'Head office or registered/trading name' : ['Head office', 'registered name', 'trading name'],
+'Head Office and all United Kingdom branches' : ['all United Kingdom branches'],
+'Subsidiary of a parent company' : ['Subsidiary'],
+'Parent company with subsidiaries' : ['Parent company']
+}
+
+soc_dict = {
+     'SOC' : ['Standard Occupational Classification' , 'SOC']
+}
+
+docs_list = [
+    'tax return','Lease Agreement','Franchise Agreement', 'Business premises ownership','Criminal Record','evidence',                           'letter','Stock Exchange listing','Hierarchy chart'
+]
+docs_dict = {
+    'Employer’s liability insurance cover': ['insurance cover', 'employer’s liability'],
+    'Corporate/Business Bank Statement' : ['bank statement'],
+    'Company Tax Return CT620': ['CT620'],
+    'Company Tax Return CT600': ['CT600'],
+    'Company Tax Return CT603': ['CT603'],
+    'Copies of advertisements' : ['advertisment'],
+    'Complete list of Candidates and their assessment' : ['list of candidates'],
+    'Latest submitted accounts': ['submitted account'],
+    'Audited annual accounts': ['annual account'],
+    'Certificate of VAT registration': ['VAT registration'],
+    'PAYE & National Insurance registration': ['PAYE registration', 'Insurance registration']
+ } 
+
+relation_list = [
+    'family', 'husband', 'wife', 'spouse', ' parent', 'grandparent', 'child', 'partner', ' married', 'unmarried', 'dependent relative',
+    'relationship'
+]
+
+general_list = [
+    'salary', 'financial requirement', 'duration', 'age', 'points', 'endorsement', 'permitted paid engagements', 'visa waiver', 
+    'transit visitor', 'sponsor', 'highly skilled'
+]
+                
+
+reference_list = [
+    'Appendix A', 'Appendix AR', 'Appendix AR (EU)', 'Appendix Armed Forces', 'Appendix B', 'Appendix C', 'Appendix D', 'Appendix E',
+    'Appendix ECAA', 'Appendix EU', 'Appendix EU (family permit)', 'Appendix F', 'Appendix FM', 'Appendix FM-SE', 'Appendix G',
+    'Appendix H', 'Appendix J', 'Appendix K', 'Appendix KoLL', 'Appendix M', 'Appendix N', 'Appendix O', 'Appendix P', 'Appendix SN',
+    'Appendix T','Appendix U', 'Appendix V', 'Appendix W', 'the Immigration Acts', 'the 1993 Act', 'the 1996 Act',
+    'the 2006 EEA Regulations', 'Refugee Convention'
+]
+
+
+ 
